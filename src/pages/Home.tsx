85171807c79018c104c2280a8617ee6c758e49bf
AlexandreGoular <xandeaugustogoulart@gmail.com>
2024-08-08 11:25:36 -0300

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Aside } from "../components/Aside";
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
 import { Section } from "../components/Section";
+import '../styles/Index.scss'
 
 export function Home() {
 
